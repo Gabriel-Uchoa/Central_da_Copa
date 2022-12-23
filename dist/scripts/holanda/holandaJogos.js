e1fad1924b3ed88098bf70419320bf9bca6a53cd
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-12-22 21:55:44 -0300

    page jogos finalizada!

--- dist/scripts/holanda/holandaJogos.js
+++ dist/scripts/holanda/holandaJogos.js
@@ -8,6 +8,10 @@
 //     fase!: string;
 // }
 let team = 'Holanda';
+const btn = document.querySelector('.btnMost');
+btn.addEventListener('click', function () {
+    divExpand();
+});
 searchAPI(team);
 async function searchAPI(team) {
     let games = await searchGamesAll();
@@ -26,15 +30,32 @@ function searchGamesTeam(games, team) {
     });
 }
 function creatElement(element) {
+    var _a;
     if (element.fase === 'Classificatória') {
         let div = document.createElement('div');
-        let p1 = document.createElement('p');
-        let p2 = document.createElement('p');
-        p1.appendChild(document.createTextNode(element.timeA, 'x', element.timeB));
-        p2.appendChild(document.createTextNode(element.gols.timeA, 'x', element.gols.timeB));
-        div.appendChild(p1);
-        div.appendChild(p2);
+        div.className = 'games';
+        div.appendChild(document.createElement('p')).innerText = `${element.timeA} X ${element.timeB}`;
+        div.appendChild(document.createElement('p')).innerText = `${element.gols.timeA} X ${element.gols.TimeB}`;
         document.querySelector(".gamesClassificatory").appendChild(div);
+    }
+    else {
+        let divQualifiers = document.createElement('div');
+        divQualifiers.className = "gamesQualifiers";
+        divQualifiers === null || divQualifiers === void 0 ? void 0 : divQualifiers.appendChild(document.createElement('p')).innerText = `${element.fase}`;
+        divQualifiers.appendChild(document.createElement('p')).innerText = `${element.timeA} X ${element.timeB}`;
+        divQualifiers.appendChild(document.createElement('p')).innerText = `${element.gols.timeA} X ${element.gols.TimeB}`;
+        (_a = document.querySelector(".stages")) === null || _a === void 0 ? void 0 : _a.appendChild(divQualifiers);
+    }
+}
+function divExpand() {
+    const expand = document.querySelector(".gamesClassificatory");
+    if (btn.innerText === "Ver Mais") {
+        expand.style.height = 50 + 'vh';
+        btn.innerText = "Ver Menos";
+    }
+    else {
+        expand.style.height = 20 + 'vh';
+        btn.innerText = "Ver Mais";
     }
 }
 //# sourceMappingURL=holandaJogos.js.map
